fdd473c7809eae877a9b5326318eaaefa58d0e4d
metaheather <heathernetheridge@gmail.com>
2021-10-27 13:54:04 -0500

    will now check user input and compare to random sentence, win messagge is hidden until all charachters match

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { lorem } from 'faker';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,17 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'typing-challenge';
+  randomSentence = lorem.sentence();
+  input ='';
+  win = false;
+
+  onInput(value: string): void {
+    this.input = value;
+
+    if(this.input === this.randomSentence) {
+      this.win = true;
+    }
+    
+  }
+
 }
